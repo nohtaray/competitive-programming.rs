f4efe68487a8114cdbf99f624c7dfb6c659986b9
Yada <ydt.hran2@gmail.com>
2019-08-08 18:35:33 +0900

    Update common.rs

--- common.rs
+++ common.rs
@@ -1,18 +1,12 @@
+use std::fs::File;
+use std::io::{stdout, BufWriter, Read, Write};
 use std::thread;
 
-/// ローカル環境なら _in.txt から入力を受ける
-fn read_input() -> String {
-    use std::fs::File;
-    use std::io::Read;
-    let mut s = String::new();
-    match option_env!("LOCAL") {
-        Some(_) => File::open("src/_in.txt")
-            .expect("File not found")
-            .read_to_string(&mut s)
-            .unwrap(),
-        None => std::io::stdin().read_to_string(&mut s).unwrap(),
-    };
-    s
+fn run() {
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+    let input = read_input();
+    let mut input = input.split_whitespace();
 }
 
 fn main() {
@@ -24,7 +18,16 @@ fn main() {
         .unwrap();
 }
 
-fn run() {
-    let input = read_input();
-    let mut input = input.split_whitespace();
+/// ローカル環境なら _in.txt から入力を受ける
+fn read_input() -> String {
+    let stdin = std::io::stdin();
+    let mut s = String::new();
+    match option_env!("LOCAL") {
+        Some(_) => File::open("src/_in.txt")
+            .expect("File not found")
+            .read_to_string(&mut s)
+            .unwrap(),
+        None => stdin.lock().read_to_string(&mut s).unwrap(),
+    };
+    s
 }
